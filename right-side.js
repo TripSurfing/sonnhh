dc707ef35f35daf236fa208a07a58dcc94b57012
Huu Son <huuhoangson235@gmail.com>
2016-07-03 02:21:15 +0700

    Last update - close repo. Move to extension repo

--- right-side.js
+++ right-side.js
@@ -7,6 +7,16 @@ var nomore = '<div class="no-more">\
                     </p>\
                 </div>'
 var rightSide = $("#right-side");
+$(function() {
+    rightSide.resizable();
+    $("#ext-nav").tabs({
+        active: 1,
+        heightStyle: "fill",
+        hide: {effect: "fade", duration: 150},
+        show: {effect: "fade", duration: 150}
+    });
+    load();
+});
 
 /*
  * Delete a box
@@ -15,34 +25,45 @@ var rightSide = $("#right-side");
 
 rightSide.on("click", ".delete-btn", function () {
     var boxParent = $(this).parents('div.box');
-    boxParent.children('div.box-container').addClass('box-blur');
+    boxParent.children('div.box-info', 'div.box-image').addClass('box-blur');
     boxParent.prepend(
         '<div class="box-confirm">\
             <div class="box-confirm-content">\
                 <p>Are you sure?</p>\
-                <button type="button" class="btn btn-warning btn-confirm-delete">Yes</button>\
-                <button type="button" class="btn btn-default btn-confirm-cancel">No</button>\
+                <button type="button" class="btn-confirm-no">No</button>\
+                <button type="button" class="btn-confirm-yes">Yes</button>\
             </div>\
         </div>');
 });
-
-rightSide.on("click", ".icon-heart", function() {
-    $(this).toggleClass("favorite-active favorite-not-active");
-});
-
-rightSide.on('click', '.btn-confirm-cancel', function() {
+rightSide.on('click', '.btn-confirm-no', function() {
     var parent = $(this).parents('div.box-confirm');
-    parent.siblings('.box-container').removeClass('box-blur');
+    parent.siblings('.box-info', '.box-image').removeClass('box-blur');
     parent.remove();
 
 });
-rightSide.on('click', '.btn-confirm-delete', function() {
-    $(this).parents('div.box').remove();
-    // $(this).parents("div.box").slideUp(400, function() {
-    //     // Sau này muốn thêm chức năng undo, bỏ dòng này đi
-    //     $(this).remove();
-    // });
+rightSide.on('click', '.btn-confirm-yes', function() {
+    // $(this).parents('div.box').remove();
+    $(this).parents("div.box").fadeOut(300, function() {
+        // Sau này muốn thêm chức năng undo, bỏ dòng này đi
+        $(this).remove();
+    });
     /*Do something to server here*/
+});
+
+rightSide.on("click", ".icon-heart", function() {
+    
+    heart = $(this);
+
+    heart.toggleClass("favorite-active favorite-not-active");
+    // if (heart.hasClass('favorite-active')) {
+    //     $.post('wwww.tripsurfing.co/api/favorite', 
+    //         {
+    //             itemId:,
+    //             type:,
+    //             action: 'add'
+    //         }
+    //     );
+    // }
 });
 
 /*
@@ -80,104 +101,81 @@ function resizeRightSide() {
     rightSide close
     */
 var  updatePlace = function(placeList) {
+	console.time('test');
     for (let place of placeList) {
         var img_url;
         place.detail == null ? img_url = "http://www.tripsurfing.co/static/img/noimg.jpg" : img_url=place.detail.url; 
         var item = 
-            '<div class="media box">\
-                <div class="box-container">\
-                    <div class="media-left">\
-                        <a href='+ place.url +' target="_blank">\
-                            <div class="media-object box-avatar-left" style="background-image: url('+img_url +')"> </div>\
-                        </a>\
-                    </div>\
-                    <div class="media-body box-body">\
+            '<div class="box">\
+                    <a class="box-image" href='+ place.url +' target="_blank">\
+                        <div class="image" style="background-image: url('+img_url +')"> </div>\
+                    </a>\
+                    \
+                    <div class="box-info">\
                         <a href='+place.url +' class="box-title" target="_blank" title="'+place.name +'">\
-                            <h4 class="media-heading title-wrap">'+place.name +'</h4>\
+                            <h4 class="ext-title-wrap">'+place.name +'</h4>\
                         </a>\
-                        <div class="box-body-content saved-places-content">'+place.address +'</div>\
-                        <div class="place-bottom">\
+                        \
+                        <div class="box-desc" title="'+place.address+'">'+place.address +'</div>\
+                        \
+                        <div class="place-bottom" id="place-'+place.id+'">\
                             <div class="rating">\
                                 <i class="icon-entypo icon-star"></i> 4.5\
                             </div>\
                             <div class="delete-btn">\
-                                <i class="icon-entyp icon-trash delete-icon" data-toggle="tooltip" data-placement="left" title="Delete"></i>\
+                                <i class="icon-entyp icon-trash" title="Delete" ext-data="tipsy"></i>\
                             </div>\
                             <div class="favorite-btn">\
-                                <i class="icon-entypo icon-heart favorite-not-active" data-toggle="tooltip" data-placement="left" title="Add to favorite"></i>\
+                                <i class="icon-entypo icon-heart favorite-not-active"  title="Add to favorite"   ext-data="tipsy"></i>\
                             </div>\
                         </div>\
                     </div>\
-                </div>\
             </div>'
 
-        $("#saved-places-id").append(item);
+        $("#place-tab").append(item);
     }
+	console.timeEnd('test');
     // if (placeList.length > 3) $("#saved-places-id").append(nomore);
 }
-var updateQuote = function(quoteList) {
-    for (let quote of quoteList) {
-        var item = '<div class="quote box">\
-                        <div class="box-container">\
-                            <p class="quote-content quote-content-short">' + quote.content +
-                            '</p>\
-                            <div class="quote-bottom" style="background:url('+quote.icon+') 0% 50% no-repeat">\
-                                <a class="quote-source" target="_blank" href=' + quote.from_url + '>' +
-                                quote.canonicalUrl + "/..." +
-                                '</a>\
-                                <a class="quote-see-more short-quote" href="#">See more</a>\
-                            </div>\
-                        </div>\
-                    </div>';
-        $("#saved-quote-id").append(item);
-    }
-    // if (quoteList.length > 3) $("#saved-quote-id").append(nomore);
-}
+
 var  updateLink = function(linkList) {
     for (let link of linkList) {
         var item = 
-        '<div class="media box">\
-            <div class="box-container">\
-                <div class="media-left">\
-                    <a href=' + link.url + ' target="_blank">\
-                        <div class="media-object box-avatar-left" style="background-image: url(' 
-                            + link.image + ')"> \
-                        </div>\
-                    </a>\
-                </div>\
-                <div class="media-body box-body">\
-                    <a href=' + link.url + ' class="box-title"  target="_blank" title="' +link.title+'">\
-                        <h4 class="media-heading title-wrap">' + link.title +
-                    
-                        '</h4>\
-                    </a>\
-                    <div class="box-body-content saved-link-desc">' +
-                        link.description+
-                    '</div>\
-                    <div class="link-bottom" style="background:url('+link.icon+') 0% 50% no-repeat">\
-                            <a class="saved-link-footer" target="_blank" href=http://' + link.canonicalUrl + '>'
+        '<div class="box">\
+            <a class="box-image" href=' + link.url + ' target="_blank">\
+                <div class="image" style="background-image: url(' + link.image + ')"></div>\
+            </a>\
+            <div class="box-info">\
+                <a href=' + link.url + ' class="box-title"  target="_blank" title="' +link.title+'">\
+                    <h4 class="media-heading ext-title-wrap">' + link.title + '</h4>\
+                </a>\
+                <div class="box-desc" title="'+link.description+'">'+link.description+'</div>\
+                \
+                <div id="link-'+link.id+'" class="link-bottom" style="background:url('+link.icon+') 0% 50% no-repeat">\
+                    <a class="link-canonical-url" target="_blank" href=http://' + link.canonicalUrl + '>'
                         + link.canonicalUrl +
-                            '</a>\
-                            <div class="delete-btn" >\
-                                <i class="icon-entyp icon-trash delete-icon" data-toggle="tooltip" data-placement="left" title="delete"></i>\
-                            </div>\
-                    <div>\
+                    '</a>\
+                    \
+                    <div class="delete-btn" >\
+                        <i class="icon-entyp icon-trash" title="Delete" ext-data="tipsy"></i>\
+                    </div>\
                 </div>\
             </div>\
         </div>';
-        $("#saved-links-id").append(item);
+        $("#link-tab").append(item);
     }
     // if (linkList.length > 3) $("#saved-links-id").append(nomore);
 }
 var announceError = function(message) {
     var error =  
         '<div class="error">\
-            <div class="error-content"><h4>'+message+'</h4></div>\
+            <div class="error-content">'+message+'</div>\
             <a href="http://tripsurfing.co/login" target="_blank">\
-                <button class="btn btn-warning">Log In</button>\
+                <button class="login-btn">Log In</button>\
             </a>\
         </div>';
-    $("#saved-places-id").append(error);
+    $("#place-tab").append(error);
+    $("#link-tab").append(error);
 }
 
 function load(tripId) {
@@ -200,9 +198,12 @@ function load(tripId) {
         switch(res.type) {
             case "success":
                 updatePlace(res.place);
-                updateQuote(res.quote);
                 updateLink(res.link);
-                $('[data-toggle="tooltip"]').tooltip();
+                $('[ext-data="tipsy"]').tipsy({
+                    gravity: 'se',
+                    fade: true,
+                    delayIn: 500
+                });
                 break;
             case "error":
                 announceError(res.message);
@@ -215,18 +216,16 @@ function load(tripId) {
 
 }
 function clearWindow(callback) {
-    $("#saved-links-id").children("div").remove();
-    $("#saved-quote-id").children("div").remove();
-    $("#saved-places-id").children("div").remove();
+    $("#link-tab").children("div").remove();
+    $("#place-tab").children("div").remove();
     callback();
 }
+
 $('#btn-tool-show').click(function() {
-    if ($('#btn-tool-show').hasClass("not-show")) {
-        clearWindow(load);
-        // load(); 
-    }
-    
-    // reload(function() {load();});
+    // if ($('#btn-tool-show').hasClass("not-show")) {
+    //     clearWindow(load);
+    //     // load(); 
+    // }
     rightSide.slideToggle(700,"swing", function() {
         $('#btn-tool-show').toggleClass('not-show show');
     });
